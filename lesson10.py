30394e7bac5c33212f8b600d794e97f8935b3610
Muneeba Tariq <tariqmuneeba9@gmail.com>
2026-08-03 13:36:27 -0600

    Complete Python Class 3: elif and grade calculator

---
+++ lesson10.py
@@ -0,0 +1,9 @@
+marks = int(input("Enter your marks:"))
+if marks >= 90:
+    print("Grade A")
+elif marks >= 80:
+    print("Grade B")
+elif marks >= 70:
+    print("Grade C")
+else:
+    print("Fail")
