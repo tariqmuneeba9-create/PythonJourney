d60c96d88a8b7effd9f55ca9046ce0e774ef0d9b
Muneeba Tariq <tariqmuneeba9@gmail.com>
2026-08-02 21:21:15 -0600

    Add Lesson 7 and calculator program

---
+++ calculator.py
@@ -0,0 +1,8 @@
+num1 = int(input("Enter your first number: "))
+num2 = int(input("Enter your sscond number: "))
+
+
+print("Addition:", num1 + num2)
+print("substriction:", num1 - num2)
+print("multiplication:", num1 * num2)
+print("division:", num1/num2)
---
+++ lesson7.py
@@ -0,0 +1,7 @@
+a = 5
+b = 10
+
+print("addition:",a + b)
+print("substraction:",a - b)
+print("multiplication:",a * b)
+print("division:",a/b)
